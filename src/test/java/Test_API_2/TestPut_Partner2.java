744db20f67f3698a6a48916db3e42f676d656dcc
Alishba3668 <f223668@cfd.nu.edu.pk>
2024-11-23 19:41:56 +0500

    Added TestPut file in Maven Project

--- src/test/java/Test_API_2/TestPut_Partner2.java
+++ src/test/java/Test_API_2/TestPut_Partner2.java
@@ -1,9 +1,33 @@
 package Test_API_2;
 
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
 public class TestPut_Partner2 {
+    @Test
+    void TC3() {
+        String url = "https://jsonplaceholder.typicode.com/posts/1";
 
-	public TestPut_Partner2() {
-		// TODO Auto-generated constructor stub
-	}
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("id", 1);
+        requestBody.put("title", "Updated Title");
+        requestBody.put("body", "Updated content of the post.");
+        requestBody.put("userId", 1);
 
+        Response response = RestAssured.given()
+                                       .contentType(ContentType.JSON)
+                                       .body(requestBody)
+                                       .put(url);
+
+        System.out.println("Status Code: " + response.getStatusCode());
+        System.out.println("Response Body: " + response.getBody().asString());
+
+        Assert.assertEquals(200, response.getStatusCode());
+        Assert.assertEquals(response.jsonPath().getString("title"), "Updated Title");
+        Assert.assertEquals(response.jsonPath().getString("body"), "Updated content of the post.");
+    }
 }
