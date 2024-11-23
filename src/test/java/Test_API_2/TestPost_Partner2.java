40d91a58cb4124d847a7c456b4f1a0dfd0dcd5e2
Alishba3668 <f223668@cfd.nu.edu.pk>
2024-11-23 19:35:45 +0500

    Added TestPost file in Maven Project

--- src/test/java/Test_API_2/TestPost_Partner2.java
+++ src/test/java/Test_API_2/TestPost_Partner2.java
@@ -1,9 +1,32 @@
 package Test_API_2;
 
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
 public class TestPost_Partner2 {
+    @Test
+    void TC2() {
+        String url = "https://jsonplaceholder.typicode.com/posts";
 
-	public TestPost_Partner2() {
-		// TODO Auto-generated constructor stub
-	}
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("title", "New Post");
+        requestBody.put("body", "This is a new post.");
+        requestBody.put("userId", 1);
 
+        Response response = RestAssured.given()
+                                       .contentType(ContentType.JSON)
+                                       .body(requestBody)
+                                       .post(url);
+
+        System.out.println("Status Code: " + response.getStatusCode());
+        System.out.println("Response Body: " + response.getBody().asString());
+
+        Assert.assertEquals(201, response.getStatusCode());
+        Assert.assertEquals(response.jsonPath().getString("title"), "New Post");
+        Assert.assertEquals(response.jsonPath().getString("body"), "This is a new post.");
+    }
 }
