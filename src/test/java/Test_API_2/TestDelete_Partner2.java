4b11103dd66c35cd0af771b93f497e7821c99277
Alishba3668 <f223668@cfd.nu.edu.pk>
2024-11-23 19:47:49 +0500

    Added TestDelete file in Maven Project

--- src/test/java/Test_API_2/TestDelete_Partner2.java
+++ src/test/java/Test_API_2/TestDelete_Partner2.java
@@ -1,9 +1,24 @@
 package Test_API_2;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 public class TestDelete_Partner2 {
+    @Test
+    void TC4() {
+        String url = "https://jsonplaceholder.typicode.com/posts/1";
 
-	public TestDelete_Partner2() {
-		// TODO Auto-generated constructor stub
-	}
+        Response response = RestAssured.given()
+                                       .delete(url);
 
+        System.out.println("Status Code: " + response.getStatusCode());
+
+        Assert.assertEquals(200, response.getStatusCode());
+
+        Response getResponse = RestAssured.given()
+                                          .get(url);
+        Assert.assertEquals(200, getResponse.getStatusCode());
+    }
 }
